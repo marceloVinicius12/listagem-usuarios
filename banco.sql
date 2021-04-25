11efb5d8147ca3adadec034406afeb35d4ae7b2a
marcelo vinicius <viniciusmarcello12@gmail.com>
2021-04-25 20:26:49 -0300

    criacao da tabelas e conexao com o bd

---
+++ banco.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE bd_usuarios;
+CREATE TABLE tb_usuarios;
+( usuario VARCHAR(20), senha VARCHAR(8), administrador VARCHAR(3));
---
+++ conexao.php
@@ -0,0 +1,12 @@
+<?php
+
+$servidor = 'localhost';
+$usuario = 'root';
+$senha = '';
+$banco = 'bd_usuarios';
+$conexao = mysqli_connect($servidor, $usuario, $senha, $banco );
+if(!$conexao){
+    die("<br> nao conectou!!! ERRO : " .
+  mysqli_connect_error());
+}
+?>
